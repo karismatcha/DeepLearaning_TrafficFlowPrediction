c01b8dc1fc27505e935a4f71bc4a2ed469986a32
Karis Matchaparn <oob130@hotmail.com>
2017-07-27 21:48:13 +0800

    Final

--- RBM_traffic_prediction.py
+++ RBM_traffic_prediction.py
@@ -39,25 +39,27 @@ def build_model():
         df=pd.read_csv(filename+'.csv', names=['systemtime', 'Var1', 'var2'],sep=';',parse_dates=[0]) #or:, infer_datetime_format=True)
         fileDATES=df.T.to_dict().values()#export the data frame to a python dictionary
         return fileDATES #return the dictionary to work with it outside the function
-    fileDATES = importdict('C:\Users\oob13\Desktop\Internship\TrafficFlowPrediction\clustering2')
+    fileDATES = importdict('C:\Users\user\Desktop\DeepLearaning_TrafficFlowPrediction\clustering2')
+    #get time and keep it in list
+    #use time to filter data later
     timebuffer = []
     for i in range(1,len(fileDATES)):
         timebuffer.append((fileDATES[i]['systemtime'].split(","))[2]) #append only time into list #A
-    #load_data = genfromtxt('.\\clustering.csv', delimiter=',')[1:5185,-3]
     
-    CarsSpeed = genfromtxt('C:\Users\oob13\Desktop\Internship\TrafficFlowPrediction\clustering2.csv', delimiter=',')[1:,-3]
+    
+    #load any features
+    CarsSpeed = genfromtxt('C:\Users\user\Desktop\DeepLearaning_TrafficFlowPrediction\clustering2.csv', delimiter=',')[1:,-3]
     #CarsTotal = genfromtxt('C:\Users\oob13\Desktop\Internship\TrafficFlowPrediction\clustering2.csv', delimiter=',')[1:,4]
     #hol = genfromtxt('C:\Users\oob13\Desktop\Internship\TrafficFlowPrediction\clustering2.csv', delimiter=',')[1:,-7]
-    #week_read = genfromtxt('C:\Users\oob13\Desktop\Internship\TrafficFlowPrediction\clustering2.csv', delimiter=',')[1:,-8]
     
     
-    
+    #use all speed
     speed = np.array(CarsSpeed)
     
+    
     '''
-    
     #filter data in time range 6.00am to 9.00am
-    
+    #open comment depends on feature used
     #get speed since 6.00 am to 9.00 am
     speed = []
     for i in range(0,len(timebuffer)):
@@ -86,25 +88,17 @@ def build_model():
                 holiday.append(hol[i])
                 i+=1
     holiday = np.array(holiday)
-    
-    #get holiday data since 6.00 am to 9.00 am
-    week = []
-    for i in range(0,len(timebuffer)):
-        if timebuffer[i] == '6:00':
-            while timebuffer[i] != '9:05':
-                week.append(week_read[i])
-                i+=1
-    week = np.array(week)
+
     '''
+    
     #combine speed and number of car into dataset 2d array
-    #get dataset = [speed,num_car,holiday]
+    #get dataset = [100] -> [[100]]
     dataset = np.array([[]])
     for i in range(0,len(speed)):
         buffer = np.array([])
         buffer = np.append(buffer,(speed[i]))
         #buffer = np.append(buffer,round(num_car[i]))
         #buffer = np.append(buffer,round(holiday[i]))
-        #buffer = np.append(buffer,round(week[i]))
         buffer2 = np.array([buffer])
         if i == 0:
             dataset = buffer2
@@ -112,37 +106,27 @@ def build_model():
             dataset = np.concatenate((dataset,buffer2))
     dataset = (np.asarray(dataset, 'float32'))
     
-    '''
-    rescale = np.array([[]])
-    for i in range(0,dataset.shape[0]):
-        buffer = np.array([])
-        buffer = np.append(buffer,(dataset[i,0] - np.min(speed)) / (np.max(speed)-np.min(speed)))
-        buffer = np.append(buffer,(dataset[i,1] - np.min(num_car)) / (np.max(num_car)-np.min(num_car)))
-        buffer = np.append(buffer,(dataset[i,2] - np.min(holiday)) / (np.max(holiday)-np.min(holiday)))
-        buffer = np.append(buffer,(dataset[i,3] - np.min(week)) / (np.max(week)-np.min(week)))
-        buffer2 = np.array([buffer])
-        if i == 0:
-            rescale = buffer2
-        else:
-            rescale = np.concatenate((rescale,buffer2))
-    '''        
+    
+    #change all value from real value to 0-1
+    #use equation z = (x-avg(x))/sd(x)
     rescale = np.array([[]])
     for i in range(0,dataset.shape[0]):
         buffer = np.array([])
         buffer = np.append(buffer,(dataset[i,0] - np.mean(speed)) / (np.std(speed)))
         #buffer = np.append(buffer,(dataset[i,1] - np.mean(num_car)) / (np.std(num_car)))
         #buffer = np.append(buffer,(dataset[i,2] - np.mean(holiday)) / (np.std(holiday)))
-        #buffer = np.append(buffer,(dataset[i,3] - np.mean(week)) / (np.std(week)))
         buffer2 = np.array([buffer])
         if i == 0:
             rescale = buffer2
         else:
             rescale = np.concatenate((rescale,buffer2))
     
+    #divide training data and testing data
     train_ratio = 0.75
     divider = int(round(train_ratio*rescale.shape[0]))
     
-    pred_minutes = 5
+    #set future minutes for predicting
+    pred_minutes = 60
     
     #divide data into train and test
     X_train = rescale[:divider-int(pred_minutes/5)]
@@ -166,10 +150,10 @@ def build_model():
     train_model = Sequential()
     
     train_model.add(rbm)
-    #train_model.add(Dense(1, activation='sigmoid'))
     
     
-    #train_model.summary()
+    #set optimizer as Stochastic gradient descent
+    #set loss fnction as contrastive divergence loss
     opt = SGD(lr, 0., decay=0.0, nesterov=False)
     loss=rbm.contrastive_divergence_loss
     metrics = [rbm.reconstruction_loss]
@@ -183,36 +167,26 @@ def build_model():
     train_model.fit(X_train, Y_train, batch_size, nb_epoch, 
     		    verbose=1, shuffle=False)
     
-    
-    
     # generate hidden features from input data
     print('Creating inference model...')
-    
     h_given_x = rbm.get_h_given_x_layer(as_initial_layer=True)
     
+    #add output layer to model
     inference_model = Sequential()
-    #inference_model.add(Dense(6, input_dim = 2, activation='relu'))
     inference_model.add(h_given_x)
-    #inference_model.add(Dense(8, activation='relu'))
-    #inference_model.add(SampleBernoulli(mode='maximum_likelihood'))
     
     print('Compiling Theano graph...')
     inference_model.compile(opt, loss='mean_squared_error')
     
+    #predicting result
+    #get 0-1 values
     print('Doing inference...')
     h = inference_model.predict(X_test)
     print(h)
     
-    '''
     #convert result to real speed
-    speed_result = []
-    for i in range(0,len(h)):
-        speed_result.append(round((h[i,0]*(np.max(speed)-np.min(speed)) + np.min(speed)))) #transfrom all predicted value into speed value
-    speed_result = np.array(speed_result)
-    print(speed_result)
-    '''
-    
-    #convert result to real speed
+    #use invert of the same equation
+    #speed_result var is the predicted speed after values are transformed 
     speed_result = []
     for i in range(0,len(h)):
         speed_result.append(round((h[i,0]*(np.std(speed)) + np.mean(speed)))) #transfrom all predicted value into speed value
@@ -220,97 +194,34 @@ def build_model():
     print(speed_result)
     
     
-    '''
-    
-    range_sd = genfromtxt('.\\clustering.csv', delimiter=',')[1:,-5]
-    check_count = 0
-    ##############################################
-    for i in range(0,len(timebuffer)-1):
-        sd = []
-        j=0
-        if timebuffer[i] == '6:00': #This is 1 day data
-            while timebuffer[i+j] != '9:05':
-                print(i)
-                sd.append(abs(range_sd[i+j]))
-                j+=1
-            sd = np.array(sd)
-            threshold = np.mean(sd)
-            threshold = math.ceil(threshold)
-            if(threshold <= 5):
-                threshold = 5
-            #print(threshold)
-        for j in range(0,37):
-            check = abs(Y_test[i+j,0] - speed_result[i+j]) > abs(threshold)
-            if check == True:
-                    check_count+=1
-    accuracy = (float(speed_result.shape[0]-check_count)/speed_result.shape[0])*100
-    print("RBM Prediction Accuracy = %.2f %%" % accuracy)
-    
-    ##############################################
-    '''
-    
     
     #Evaluation part
+    #find accuracy of model by using threshold
     #set speed difference threshold
-    #threshold = genfromtxt('.\\clustering.csv', delimiter=',')[1:,-5]
     threshold = 5
-    mse_buffer = 0
-    mae_buffer = 0
     check_count = 0
     for i in range(0,speed_result.shape[0]):
         check = abs(Y_test[i,0] - speed_result[i]) > abs(threshold)
-        mse_buffer += (Y_test[i,0] - speed_result[i])*(Y_test[i,0] - speed_result[i])
-        mae_buffer += abs(Y_test[i,0] - speed_result[i])
         if check == True:
             check_count+=1
     accuracy = (float(speed_result.shape[0]-check_count)/speed_result.shape[0])*100
     print("RBM Prediction Accuracy = %.2f %%" % accuracy)
-    
-    
-    mse = math.sqrt(mse_buffer/speed_result.shape[0])
-    mae = mae_buffer/speed_result.shape[0]
-    print("MSE = %.2f %%" % mse)
-    print("MAE = %.2f %%" % mae)
-    
     print('Done!')
     
-    '''
+    #find root mean square error
+    def rmse(predictions,targets):
+        return (np.sqrt(((predictions-targets)**2).mean()))
+    print ("MSE")
+    print (rmse(speed_result, Y_test[:,0]))
     
-    less_than_5 = 0
-    less = 0
-    more = 0
-    more_than_5 = 0
-    equal = 0
-    for i in range(0,speed_result.shape[0]):
-        if (speed_result[i] - Y_test[i,0]) < -threshold and (speed_result[i] - Y_test[i,0]) < 0:
-            less_than_5 = less_than_5 +1
-        elif (speed_result[i] - Y_test[i,0]) >= -threshold and (speed_result[i] - Y_test[i,0]) < 0:
-            less = less +1
-        elif (speed_result[i] - Y_test[i,0]) == 0:
-            equal = equal +1
-        elif (speed_result[i] - Y_test[i,0]) <= threshold and (speed_result[i] - Y_test[i,0]) > 0:
-            more = more +1
-        elif (speed_result[i] - Y_test[i,0]) > threshold and (speed_result[i] - Y_test[i,0]) > 0:
-            more_than_5 = more_than_5 +1
+    #find mean absolute error
+    def mae(predictions,targets):
+        return ((np.absolute(predictions-targets)).mean())
     
-    less_than_5 = (less_than_5/speed_result.shape[0])*100
-    less = (less/speed_result.shape[0])*100
-    equal = (equal/speed_result.shape[0])*100
-    more = (more/speed_result.shape[0])*100
-    more_than_5 = (more_than_5/speed_result.shape[0])*100
-    
-    print("outbound lower = %.2f %% " % less_than_5)
-    print("lower = %.2f %%" % less)
-    print("equal = %.2f %%" % equal)
-    print("higher = %.2f %%" % more)
-    print("outbound higher = %.2f %%" % more_than_5)
-    
-    
-    print('Done!')
-    '''
-    
-    
-    
+    print ("MAE")
+    print (mae(speed_result, Y_test[:,0]))
+      
+    #save ground-truth value and predicted value to csv
     with open('speed.csv', 'wb') as f:
          writer = csv.writer(f, delimiter = ',')
          for row in Y_test:
@@ -322,45 +233,7 @@ def build_model():
     
     
     
-    
-    
-    
-    
-    '''
-    #get user input and predict the next speed
-    min_speed = float(np.min(speed))
-    max_speed = float(np.max(speed))
-    min_numcar = float(np.min(num_car))
-    max_numcar = float(np.max(num_car))
-    min_hol = float(np.min(holiday))
-    max_hol =  float(np.max(holiday))
-    min_week = float(np.min(week))
-    max_week =  float(np.max(week))
-    
-    def get_input():
-        input_speed = input('Enter speed: ')
-        input_numcar = input('Enter number of car: ')
-        input_hol = input('Enter holday(0 = no, 1 = yes): ')
-        input_week = input('Enter week(1 = Monday): ')
-        if(input_speed != -1 and input_numcar != -1 and input_hol!=-1 and input_week != -1):
-            input_speed = (input_speed - min_speed) / (max_speed-min_speed)
-            input_numcar = (input_numcar - min_numcar) / (max_numcar-min_numcar)
-            input_hol = (input_hol - min_hol) / (max_hol-min_hol)
-            input_week = (input_week - min_week) / (max_week-min_week)
-            buffer = np.array([[input_speed,input_numcar,input_hol,input_week]])
-            h = inference_model.predict(buffer)
-            
-            result = round(h[0,0]*(max_speed-min_speed) + min_speed)
-            print ('Next Speed is ' + str(result))
-            return 1
-        else:
-            return -1
-    out = 1
-    while(out!=-1):
-        print()
-        print("Enter speed = -1 , number of car = -1 , holiday = -1 and week = -1 to exit")
-        out = get_input()
-    '''
+
 
 if __name__ == '__main__':
     build_model()
